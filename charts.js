af184b5a81941a25b579b35e129d70504d716f53
Matthew DeYoung <deyoungmatthewd@gmail.com>
2021-05-16 21:15:48 -0400

    Finally got the stupid gauge chart to load because I couldn't spell 'guage' correctlygit add -Agit add -A :(

--- charts.js
+++ charts.js
@@ -34,9 +34,11 @@ function optionChanged(newSample) {
 function buildMetadata(sample) {
   d3.json("samples.json").then((data) => {
     var metadata = data.metadata;
+
     // Filter the data for the object with the desired sample number
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
     var result = resultArray[0];
+
     // Use d3 to select the panel with id of `#sample-metadata`
     var PANEL = d3.select("#sample-metadata");
 
@@ -70,8 +72,8 @@ function buildCharts(sample) {
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
     var otu_ids = first.otu_ids;
-    var otu_labels = first.otu_labels.slice(0,10).reverse();
-    var sample_values = first.sample_values.slice(0,10).reverse();
+    var otu_labels = first.otu_labels;
+    var sample_values = first.sample_values;
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
@@ -82,8 +84,8 @@ function buildCharts(sample) {
     // 8. Create the trace for the bar chart. 
     var barData = [{
       y: yticks,
-      x: sample_values,
-      text: otu_labels,
+      x: sample_values.slice(0,10).reverse(),
+      text: otu_labels.slice(0,10).reverse(),
       type: "bar",
       orientation: "h",
       }];
@@ -120,45 +122,43 @@ function buildCharts(sample) {
     
     // 1. Create a variable that filters the metadata array for the object with the desired sample number.
     var metadata = data.metadata;
-
+    var gaugeArray = metadata.filter(metaObj => metaObj.id == sample);
     // Create a variable that holds the first sample in the array.
-    var gaugeFirstArray = metadata.filter(metaObj => metaObj.id == sample);
+    
 
     // 2. Create a variable that holds the first sample in the metadata array.
-    var guageFirstMeta = gaugeFirstArray[0];
+    var gaugeMeta = gaugeArray[0];
 
     // 3. Create a variable that holds the washing frequency.
-   var washFreq = guageFirstMeta.wfreq;
+    var washFreq = gaugeMeta.wfreq;
 
         
     // 4. Create the trace for the gauge chart.
     var gaugeData = [{
       value: washFreq,
-      title: "BELLY BUTTON WASHING FREQUENCY",
       type: "indicator",
-      mode: "guage+number",
-      guage: {
-        axis: { range: [null, 10], tickwidth: 1, tickcolor: "darkblue"},
-        bar: { color: "darkblue"},
-        bgcolor: "white",
-        borderwidth: 2,
-        steps: [
+      mode: "gauge+number",
+      title: {text: "<b> BELLY BUTTON WASHING FREQUENCY </b> <br></br> SCRUBS PER WEEK"},
+      gauge: {
+        axis: {range: [null,10], dtick: "2"},
+        bar: {color: "black"},
+        steps:[
           {range: [0, 2], color: "red"},
           {range: [2, 4], color: "darkorange"},
           {range: [4, 6], color: "yellow"},
           {range: [6, 8], color: "chartreuse"},
           {range: [8, 10], color: "darkgreen"}
-        ]
+        ],
       }
-    }
-     
-    ];
+    }];
     
     // 5. Create the layout for the gauge chart.
     var gaugeLayout = { 
-     
+      automargin: true
     };
 
     // 6. Use Plotly to plot the gauge data and layout.
+    Plotly.newPlot("gauge", gaugeData, gaugeLayout);
+
   });
 }
